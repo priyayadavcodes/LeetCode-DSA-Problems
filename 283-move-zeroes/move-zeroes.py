b933b8dca87176a716ba827be7a01813158b9556
priyayadavcodes <110210563+priyayadavcodes@users.noreply.github.com>
2024-05-28 15:08:38 +0530

    Time: 724 ms (10.98%) | Memory: 17.9 MB (96.16%) - LeetSync

--- 283-move-zeroes/move-zeroes.py
+++ 283-move-zeroes/move-zeroes.py
@@ -3,9 +3,9 @@ class Solution:
         n = len(nums)
         for i in range(n):
             if nums[i] == 0:
-                zero = nums[i]
-                nums.append(zero)
-                nums.remove(zero)
+           
+                nums.append(nums[i])
+                nums.remove(nums[i])
             
 
         
