c0e05c5b48fce78302f8c1bb4728399bab75cff0
priyayadavcodes <110210563+priyayadavcodes@users.noreply.github.com>
2024-06-05 10:57:47 +0530

    Time: 37 ms (62.01%) | Memory: 16.5 MB (84.05%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.py
+++ 14-longest-common-prefix/longest-common-prefix.py
@@ -1,46 +1,20 @@
+from typing import List
+
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
-        n = len(strs)
-
-        
-        if n == 1 and strs[0] == "":
+        # Check for empty list
+        if not strs:
             return ""
-        if n == 1:
-            return (''+ strs[0] +'')
-
         
-        lst = []
-        for i in range(n-1):
-            ls = strs[i]
-            m = min(len(strs[i]),len(strs[i+1]))
-            count = 0
-            if m == 1:
-                if strs[i][0] == strs[i+1][0]:
-                    lst.append(strs[i][0])
-            else:
-                
-                    
-                for j in range(m):
-                    a = strs[i][0:j+1]
-                    b = strs[i+1][0:j+1]
-                    if a == b:
-                        lsi = strs[i][0:j+1]
-                        count = count+1
-                    
-                if count>0:
-                    lst.append(lsi)
-            if len(lst) != i+1:
-                return ""
-
-        if len(lst)== 0:
-            return ""
-            
-        else:
-            length = 1000
-            for i in range(len(lst)):
-                ln = len(lst[i])
-                if ln < length:
-                    length = ln
-            v = lst[0][0:length]
-            s = str(v)
-            return s
+        # Initialize the prefix with the first string
+        prefix = strs[0]
+        
+        # Iterate over the rest of the strings
+        for string in strs[1:]:
+            # Reduce the prefix length until it matches the start of the current string
+            while not string.startswith(prefix):
+                prefix = prefix[:-1]
+                if not prefix:
+                    return ""
+        
+        return prefix
