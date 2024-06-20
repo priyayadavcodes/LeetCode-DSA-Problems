20b7d7294adc3c0f7f68d3b981b22303c88582f4
priyayadavcodes <110210563+priyayadavcodes@users.noreply.github.com>
2024-06-20 18:44:34 +0530

    Time: 951 ms (57.94%) | Memory: 62.8 MB (60.32%) - LeetSync

--- 1622-max-value-of-equation/max-value-of-equation.py
+++ 1622-max-value-of-equation/max-value-of-equation.py
@@ -1,23 +1,19 @@
 class Solution:
     def findMaxValueOfEquation(self, points: List[List[int]], k: int) -> int:
         
-        dq = deque()  # to store the points in the form (y - x, x)
+        dq = deque()  
         maxvalue = float("-inf")
         
         for x, y in points:
-            # Remove points from deque which are out of the window |x1 - x2| > k
             while dq and x - dq[0][1] > k:
                 dq.popleft()
-            
-            # Calculate the maximum value of the equation using the current point and the point in front of the deque
+
             if dq:
                 maxvalue = max(maxvalue, y + x + dq[0][0])
-            
-            # Maintain the deque in decreasing order of (y - x)
+
             while dq and y - x >= dq[-1][0]:
                 dq.pop()
             
-            # Add the current point to the deque
             dq.append((y - x, x))
         
         return maxvalue
