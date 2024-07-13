4bf5f08a51a55513d4baef864a9774b5a3869bcf
priyayadavcodes <110210563+priyayadavcodes@users.noreply.github.com>
2024-07-13 17:34:07 +0530

    Time: 1200 ms (26.08%) | Memory: 0B (100.00%) - LeetSync

--- 1882-the-number-of-employees-which-report-to-each-employee/the-number-of-employees-which-report-to-each-employee.sql
+++ 1882-the-number-of-employees-which-report-to-each-employee/the-number-of-employees-which-report-to-each-employee.sql
@@ -1,8 +1,12 @@
 SELECT 
     employee_id, 
     name, 
-    (SELECT COUNT(*) FROM Employees WHERE reports_to = e.employee_id) AS reports_count, 
-    (SELECT ROUND(AVG(age)) FROM Employees WHERE reports_to = e.employee_id) AS average_age 
+    (SELECT COUNT(*) 
+     FROM Employees 
+     WHERE reports_to = e.employee_id) AS reports_count, 
+    (SELECT ROUND(AVG(age)) 
+     FROM Employees 
+     WHERE reports_to = e.employee_id) AS average_age 
 FROM 
     Employees e
 WHERE 
