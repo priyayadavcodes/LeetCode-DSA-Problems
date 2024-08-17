92fde9dc13eec9f013438786476886c1b8b479db
priyayadavcodes <110210563+priyayadavcodes@users.noreply.github.com>
2024-08-18 01:08:44 +0530

    Time: 46 ms (15.95%) | Memory: 16.6 MB (49.08%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.py
+++ 88-merge-sorted-array/merge-sorted-array.py
@@ -1,22 +1,20 @@
-from typing import List
-
 class Solution:
-    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
-        # Initialize pointers for nums1, nums2, and the end of the merged array
-        p1 = m - 1
-        p2 = n - 1
-        p = m + n - 1
+    def merge(self, arr1: List[int], n: int, arr2: List[int], m: int) -> None:
+        i, j = n - 1, m - 1
+    
+        k = n + m - 1
 
-        # Merge the arrays starting from the end
-        while p1 >= 0 and p2 >= 0:
-            if nums1[p1] > nums2[p2]:
-                nums1[p] = nums1[p1]
-                p1 -= 1
+        while i >= 0 and j >= 0:
+            if arr1[i] > arr2[j]:
+                arr1[k] = arr1[i]
+                i -= 1
             else:
-                nums1[p] = nums2[p2]
-                p2 -= 1
-            p -= 1
+                arr1[k] = arr2[j]
+                j -= 1
+            k -= 1
+        
 
-        # If there are remaining elements in nums2, copy them over
-        nums1[:p2 + 1] = nums2[:p2 + 1]
-
+        while j >= 0:
+            arr1[k] = arr2[j]
+            j -= 1
+            k -= 1
