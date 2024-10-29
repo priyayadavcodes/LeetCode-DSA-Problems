828059d374217c8b256452193f23322f314c438f
priyayadavcodes <110210563+priyayadavcodes@users.noreply.github.com>
2024-10-29 15:46:17 +0530

    Time: 402 ms (44.89%) | Memory: 0B (100.00%) - LeetSync

--- 570-managers-with-at-least-5-direct-reports/managers-with-at-least-5-direct-reports.sql
+++ 570-managers-with-at-least-5-direct-reports/managers-with-at-least-5-direct-reports.sql
@@ -1,10 +1,12 @@
-with tab as (select managerID,
-count(managerId) as num 
-from employee
-group by managerID)
+with tab as (select
+e2.name,
+count(e1.managerId) as num
+from 
+employee e1
+join employee e2
+on e1.managerId = e2.id
+group by e1.managerId)
 
 select name
-from employee
-where id in (select managerId
-            from tab 
-            where num >=5)
+from tab 
+where num >= 5
